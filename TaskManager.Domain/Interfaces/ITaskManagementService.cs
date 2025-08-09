639c615405655e4539d3f0a7ed4f6e0cd074d050
Joe <joe.griffin@jcgriffin.io>
2025-08-09 12:23:56 +0100

    Creates domain interfaces to be consumed by other parts of code

---
+++ TaskManager.Domain/Interfaces/ITaskManagementService.cs
@@ -0,0 +1,12 @@
+﻿using TaskManager.Domain.Models.TaskModels;
+
+namespace TaskManager.Domain.Interfaces
+{
+    public interface ITaskManagementService
+    {
+        public Task<TaskModel?> CreateTaskAsync(CreateTaskModel createTaskModel);
+        public Task<TaskCollectionModel> GetTasksAsync(TaskQuery query);
+        public Task<TaskModel?> UpdateTaskAsync(UpdateTaskModel taskToUpdate);
+        public Task<bool> DeleteTaskAsync(int taskId);
+    }
+}
