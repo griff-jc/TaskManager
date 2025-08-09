30b92f84067f8d257a80915f6f7f1553318bdb14
Joe <joe.griffin@jcgriffin.io>
2025-08-09 12:44:15 +0100

    Creates new implementation of domain interfaces with empty implementations

---
+++ TaskManager.Domain/TaskManagementService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.Interfaces;
+using TaskManager.Domain.Models.TaskModels;
+
+namespace TaskManager.Domain
+{
+    public class TaskManagementService : ITaskManagementService
+    {
+        public Task<TaskModel?> CreateTaskAsync(CreateTaskModel createTaskModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTaskAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskCollectionModel> GetTasksAsync(TaskQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskModel?> UpdateTaskAsync(UpdateTaskModel taskToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskManager.Persistence.EFCore/EFCorePersistenceProvider.cs
@@ -0,0 +1,28 @@
+﻿using TaskManager.Domain.Interfaces;
+using TaskManager.Domain.Models.TaskModels;
+
+namespace TaskManager.Persistence.EFCore
+{
+    public class EFCorePersistenceProvider : IPersistenceProvider
+    {
+        public Task<TaskModel?> CreateTaskAsync(CreateTaskModel taskModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTaskAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskCollectionModel> GetTasksAsync(TaskQuery taskQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskModel?> UpdateTaskAsync(UpdateTaskModel taskModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
