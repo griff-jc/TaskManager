934ae8332ba950afaf399b39b85c4f982295acdd
Joe <joe.griffin@jcgriffin.io>
2025-08-09 12:38:13 +0100

    Adds registration of db context and connection string for postrgres

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Persistence.EFCore;
+
 namespace TaskManager
 {
     public class Program
@@ -8,6 +11,10 @@ namespace TaskManager
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<TaskManagerDbContext>(options =>
+            {
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
