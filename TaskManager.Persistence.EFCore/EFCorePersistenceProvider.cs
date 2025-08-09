7c3290c51849ef9fde5559f09c2c69bfff82c211
Joe <joe.griffin@jcgriffin.io>
2025-08-09 13:05:04 +0100

    CreateTask workflow implementation

--- TaskManager.Persistence.EFCore/EFCorePersistenceProvider.cs
+++ TaskManager.Persistence.EFCore/EFCorePersistenceProvider.cs
@@ -1,13 +1,43 @@
-﻿using TaskManager.Domain.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using TaskManager.Domain.Interfaces;
 using TaskManager.Domain.Models.TaskModels;
+using TaskManager.Persistence.EFCore.DbModels;
 
 namespace TaskManager.Persistence.EFCore
 {
     public class EFCorePersistenceProvider : IPersistenceProvider
     {
-        public Task<TaskModel?> CreateTaskAsync(CreateTaskModel taskModel)
+        private readonly TaskManagerDbContext _dbContext;
+        private readonly ILogger<EFCorePersistenceProvider> _logger;
+
+        public EFCorePersistenceProvider(TaskManagerDbContext dbContext, ILogger<EFCorePersistenceProvider> logger)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public async Task<TaskModel?> CreateTaskAsync(CreateTaskModel taskModel)
+        {
+            try
+            {
+                var dbTask = (DbTaskModel)taskModel;
+                _dbContext.Tasks.Add(dbTask);
+                var resultsChanged = await _dbContext.SaveChangesAsync();
+                if (resultsChanged > 0)
+                {
+                    return dbTask;
+                }
+                else
+                {
+                    _logger.LogWarning("No changes were made when trying to create the task: {taskTitle}.", taskModel.Title);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating the task: {taskTitle}.", taskModel.Title);
+                return null;
+            }
         }
 
         public Task<bool> DeleteTaskAsync(int taskId)
---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Controllers.ControllerModels;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskManagementService _taskManagementService;
+
+        public TaskController(ITaskManagementService taskManagementService)
+        {
+            _taskManagementService = taskManagementService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTaskAsync([FromBody] CreateTaskRequest createTaskModel)
+        {
+            if (createTaskModel == null)
+            {
+                return BadRequest("Task model cannot be null.");
+            }
+            try
+            {
+                var createdTask = await _taskManagementService.CreateTaskAsync(createTaskModel);
+                if (createdTask == null)
+                {
+                    return Problem(detail: "Failed to create task", statusCode: 500, title: "An error occurred while creating the task");
+                }
+                return Created($"api/task/{createdTask.Id}", createdTask);
+            } catch (Exception ex)
+            {
+                return Problem(detail: ex.Message, statusCode: 500, title: "An error occurred while creating the task");
+            }
+    }
+}
