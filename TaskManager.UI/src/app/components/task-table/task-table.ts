1659b99f0e2c836026f5d4c93b3f1cda4028a36b
Joe <joe.griffin@jcgriffin.io>
2025-08-09 15:39:48 +0100

    Adds placeholder methods into component script, also creates model for table usage

--- TaskManager.UI/src/app/components/task-table/task-table.ts
+++ TaskManager.UI/src/app/components/task-table/task-table.ts
@@ -5,7 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Task } from '../../models/Task';
 
 @Component({
   selector: 'app-task-table',
@@ -14,5 +15,29 @@ import { MatTableModule } from '@angular/material/table';
   styleUrl: './task-table.scss'
 })
 export class TaskTable {
+  public displayedColumns: string[] = ['select', 'id', 'title', 'description', 'isCompleted', 'dueDate', 'createdById', 'assignedToId', 'actions'];
+  public dataSource = new MatTableDataSource<Task>();
+  
+  goToTaskDetails(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  formatDate(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  toggleTaskCompletion(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  toggleRowSelection(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  isRowSelected(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  removeData() {
+    throw new Error('Method not implemented.');
+  }
+  addData() {
+    throw new Error('Method not implemented.');
+  }
 
 }
