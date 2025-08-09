13e2592504cd52f4e56632d9f60504b9aceb8003
Joe <joe.griffin@jcgriffin.io>
2025-08-09 16:32:39 +0100

    Creates skeleton component for task creation

---
+++ TaskManager.UI/src/app/components/task-create/task-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-create',
+  imports: [],
+  templateUrl: './task-create.html',
+  styleUrl: './task-create.scss'
+})
+export class TaskCreate {
+
+}
