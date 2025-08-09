d2dbf3999069d606631afe39537b9642937a503e
Joe <joe.griffin@jcgriffin.io>
2025-08-09 13:27:12 +0100

    Adds project infra setup specifics such as OTEL, and persistence provider factory

---
+++ TaskManager/Infrastructure/OpenTelemetrySetup.cs
@@ -0,0 +1,37 @@
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace TaskManager.Infrastructure
+{
+    internal static class OpenTelemetrySetup
+    {
+        public static void AddOpenTelemetry(this WebApplicationBuilder builder)
+        {
+            var resourceBuilder = ResourceBuilder.CreateDefault().AddService("TaskManagementService", serviceVersion: "1.0.0");
+            builder.Logging.AddOpenTelemetry(options =>
+            {
+                options.SetResourceBuilder(resourceBuilder)
+                    .AddConsoleExporter();
+            });
+            builder.Services.AddOpenTelemetry().WithMetrics(metrics =>
+            {
+                metrics.SetResourceBuilder(resourceBuilder)
+                    .AddRuntimeInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddProcessInstrumentation()
+                    .AddConsoleExporter();
+            })
+            .WithTracing(tracing =>
+            {
+                tracing.SetResourceBuilder(resourceBuilder)
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddConsoleExporter();
+            });
+        }
+    }
+}
---
+++ TaskManager/Infrastructure/PersistenceProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using TaskManager.Domain.Interfaces;
+using TaskManager.Domain.Models;
+using TaskManager.Persistence.EFCore;
+
+namespace TaskManager.Infrastructure
+{
+    public class PersistenceProviderFactory : IPersistenceProviderFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public PersistenceProviderFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IPersistenceProvider CreateProvider(PersistenceProviders persistenceProvider)
+        {
+           return persistenceProvider switch
+            {
+                PersistenceProviders.PostGreSQL => _serviceProvider.GetRequiredService<EFCorePersistenceProvider>(),
+                // Add other persistence providers here as needed
+                _ => throw new NotSupportedException($"Persistence provider {persistenceProvider} is not supported.")
+            };
+        }
+    }
+}
