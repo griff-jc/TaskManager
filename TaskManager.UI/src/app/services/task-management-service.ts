50f80f2b99ce6c2d3534d83846ff3d00b9a45a23
Joe <joe.griffin@jcgriffin.io>
2025-08-09 16:04:10 +0100

    Adds basic implementation of task management service

--- TaskManager.UI/src/app/services/task-management-service.ts
+++ TaskManager.UI/src/app/services/task-management-service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task, TaskCollectionResponse, TaskCreateRequest, TaskQueryRequest, TaskUpdateRequest } from '../models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,40 @@ export class TaskManagementService {
 
   constructor(private http: HttpClient) { }
 
-  createTask() {
-
+  createTask(request: TaskCreateRequest): Observable<Task> {
+    return this.http.post<Task>(`${this.baseUrl}`, request);
   }
 
-  getTasks() { }
+  getTasks(request: TaskQueryRequest): Observable<TaskCollectionResponse> {
+    const params = this.buildQueryParams(request);
+    return this.http.get<TaskCollectionResponse>(this.baseUrl, { params });
+  }
 
-  getTaskById(taskId: number) { }
+  getTaskById(taskId: number): Observable<Task> {
+    return this.http.get<Task>(`${this.baseUrl}/${taskId}`);
+  }
 
-  updateTask() { }
+  updateTask(request: TaskUpdateRequest): Observable<Task> {
+    return this.http.put<Task>(`${this.baseUrl}/${request.id}`, request);
+  }
 
-  deleteTask() { }
+  deleteTask(taskId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${taskId}`);
+  }
+
+  private buildQueryParams(request: TaskQueryRequest): HttpParams {
+    const params: { [key: string]: string } = {};
+    
+    if (request.id !== undefined) params['id'] = request.id.toString();
+    if (request.isCompleted !== undefined) params['isCompleted'] = request.isCompleted.toString();
+    if (request.dueDateSearchRangeStart) params['dueDateSearchRangeStart'] = request.dueDateSearchRangeStart;
+    if (request.dueDateSearchRangeEnd) params['dueDateSearchRangeEnd'] = request.dueDateSearchRangeEnd;
+
+    params['sortByTitle'] = request.sortByTitle.toString();
+    params['sortByDueDate'] = request.sortByDueDate.toString();
+    params['pageSize'] = request.pageSize.toString();
+    params['pageNumber'] = request.pageNumber.toString();
+
+    return new HttpParams({ fromObject: params });
+  }
 }
